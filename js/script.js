5b2eaac593b0a3a7b500b28dfabae072bc9c7baf
Andrea Miglietta <m.andrea96@hotmail.it>
2022-08-23 18:08:45 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 const km = prompt('How many kilometers do you want to travel??');
 const kmAsNumber = parseInt(km);
-console.log('kmAsNumber');
-const price = 0.21 * km;
+console.log(kmAsNumber.toFixed(2));
+document.getElementById("km").innerHTML += kmAsNumber.toFixed(2) + 'km';
 
-const age = prompt('How old are you?')
+const price = 0.21 * kmAsNumber;
+console.log(price.toFixed(2));
+document.getElementById("full_price").innerHTML += price.toFixed(2) + '€';
+
+
+const age = prompt('How old are you?');
 const ageAsNumber = parseInt(age);
 
 if (age <= 18) {
-    console.log('20% off')
+    let discountedPrice = price - (price * 0.2);
+    console.log(discountedPrice.toFixed(2));
+    document.getElementById("price").innerHTML += discountedPrice.toFixed(2) + '€';
+} else if (age >= 65) {
+    let discountedPrice = price - (price * 0.4);
+    console.log(discountedPrice.toFixed(2));
+    document.getElementById("price").innerHTML += discountedPrice.toFixed(2) + '€';
 } else {
-    console.log('price')
+    console.log(price.toFixed(2));
+    document.getElementById("price").innerHTML += price.toFixed(2) + '€';
 }
 
-if (age >= 65) {
-    console.log('40% off')
-} else {
-    console.log('price')
-}
+
+
+
+
